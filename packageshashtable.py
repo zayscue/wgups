f12028da07a8f95b3690d6ed9e61eb408c71096e
Zackery Ayscue <ztayscue@gmail.com>
2020-05-28 14:53:38 -0400

    added comments.

--- packageshashtable.py
+++ packageshashtable.py
@@ -9,15 +9,18 @@ class PackagesHashTable(ChainingHashTable):
     self.locations = locations
     super().__init__(initial_capacity)
 
+  # add new package to hash table
   def insert(self, package_id, delivery_address, deadline, city, zip_code, weight, delivery_status=AVAILABLE_AT_HUB):
     location = self.locations.search((delivery_address, city, zip_code))
     package = Package(package_id, location, deadline, weight, delivery_status)
     return super().insert(package)
 
+  # search for package by package id
   def search(self, package_id):
     package = super().search(package_id)
     return package
 
+  # use a set operations to filter the packages in the hash table
   def find(self, predicate):
     def get_key(el):
       return el.package_id
